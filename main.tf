1b190f4839f4659740bd0be029b04a6c439c76d1
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-03-07 12:47:04 +0900

    Added EC2 task

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 provider "aws" {
   version    = "~> 1.0"
-  access_key = "anaccesskey" // 1. Fix
-  secret_key = "asecretkey" // 1. Fix
+  region     = "sa-east-1"
+  access_key = ""          // 1. Fix
+  secret_key = ""          // 1. Fix
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-84175ae8"
+  instance_type = "t2.micro"
+  monitoring    = true
+
+  tags {
+    Name = "name" //2 fix your name
+  }
 }
