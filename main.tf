18bb19c02cc6e49509eecff8b145461f5c0eeffe
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-03-07 15:04:09 +0900

    Fixed access key

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 provider "aws" {
   version    = "~> 1.0"
   region     = "sa-east-1"
-  access_key = ""          // 1. Fix
-  secret_key = ""          // 1. Fix
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 
 resource "aws_instance" "web" {
   ami           = "ami-84175ae8"
@@ -11,6 +14,6 @@ resource "aws_instance" "web" {
   monitoring    = true
 
   tags {
-    Name = "name" //2 fix your name
+    Name = "name" // fix your name
   }
 }
